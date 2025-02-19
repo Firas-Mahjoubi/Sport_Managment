ab1ae0bcaeb25b06ad4ea14f64e153c61417b65b
Firas-Mahjoubi <mahjoubi.firas@esprit.tn>
2025-02-19 18:34:25 +0100

    ff

--- Sport_Backend/src/main/java/com/example/sport_backend/Entity/ClubHouse/User.java
+++ Sport_Backend/src/main/java/com/example/sport_backend/Entity/ClubHouse/User.java
@@ -1,11 +1,13 @@
 package com.example.sport_backend.Entity.ClubHouse;
 
 import com.example.sport_backend.Entity.Enum.Role;
-import com.example.sport_backend.Entity.TrainigGround.TrainingSession;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -13,21 +15,51 @@ import java.util.List;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level= AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String name;
     String email;
     String password;
     String phoneNumber;
+    boolean verified;
+    String verificationToken;
     @Enumerated(EnumType.STRING)
     Role role;
+
     @ManyToOne
     Team team;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> role.name());
+    }
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
