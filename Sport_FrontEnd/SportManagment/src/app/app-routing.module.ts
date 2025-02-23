c1f1282dd8a7949c3703b93d9a3dcb2823764c18
Ghassen Chahbani <chahbani.ghassen@esprit.tn>
2025-02-23 22:11:20 +0100

    front

--- Sport_FrontEnd/SportManagment/src/app/app-routing.module.ts
+++ Sport_FrontEnd/SportManagment/src/app/app-routing.module.ts
@@ -3,16 +3,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import {MatchesHomeComponent} from "./matches/matches-home/matches-home.component";
 import {GameweekSliderComponent} from "./matches/gameweek-slider/gameweek-slider.component";
+import { TrainingSessionComponent } from './components/training-session/training-session.component';
+import { ExerciseComponent } from './components/exercise/exercise.component';
+import{TrainingSessionExerciceComponent} from './components/training-session-exercice/training-session-exercice.component';
+import {TrainingSessionFormComponent} from './components/training-session-form/training-session-form.component';
+import {ExerciseFormComponent} from './components/exercise-form/exercise-form.component';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
   { path: 'reset-password', component: AuthComponent }, // Ensure reset password is mapped
   { path: '', redirectTo: '/auth', pathMatch: 'full' }, // Default route
+
+  { path: 'training-sessions', component: TrainingSessionComponent },//Training routes
+  { path: 'training-sessions-exercice', component: TrainingSessionExerciceComponent },
+  { path: 'training-sessions/new', component: TrainingSessionFormComponent }, // ✅ Add New
+  { path: 'training-sessions/edit/:id', component: TrainingSessionFormComponent }, // ✅ Edit
+
+  { path: 'exercises', component: ExerciseComponent },//exervise routes
+  { path: 'add-exercise', component: ExerciseFormComponent },
+  { path: 'edit-exercise/:id', component: ExerciseFormComponent },
+
   { path: 'matches', component: MatchesHomeComponent },
   { path: 'gameweeks', component: GameweekSliderComponent },];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],  
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
---
+++ Sport_FrontEnd/SportManagment/src/app/components/training-ground-navbar/training-ground-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-ground-navbar',
+  templateUrl: './training-ground-navbar.component.html',
+  styleUrls: ['./training-ground-navbar.component.css']
+})
+export class TrainingGroundNavbarComponent {
+
+}
