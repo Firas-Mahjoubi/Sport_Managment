34302c7008e83c084675ded3752b140ae3b448be
Maram Naouali <MaramNaoueli>
2025-02-24 15:32:30 +0100

    maram

--- Sport_Backend/src/main/java/com/example/sport_backend/Controllers/ClubHouse/TeamController.java
+++ Sport_Backend/src/main/java/com/example/sport_backend/Controllers/ClubHouse/TeamController.java
@@ -2,35 +2,40 @@ package com.example.sport_backend.Controllers.ClubHouse;
 
 import com.example.sport_backend.Entity.ClubHouse.Team;
 import com.example.sport_backend.ServiceImpl.ClubHouse.TeamServiceIMPL;
+import com.example.sport_backend.ServiceInterface.ClubHouse.ITeamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequiredArgsConstructor
 public class TeamController {
-private TeamServiceIMPL teamServiceIMPL;
+    @Autowired
+private ITeamService iTeamService;
 
 @GetMapping("/getallteams")
     public List<Team> getAllTeams() {
-        return teamServiceIMPL.getAllTeams();
+        return iTeamService.getAllTeams();
     }
 
     @GetMapping("/getteam/{id}")
     public Team getTeamById(@PathVariable Long id) {
-        return teamServiceIMPL.getTeamById(id);
+        return iTeamService.getTeamById(id);
     }
 
     @PostMapping ("/addteam")
     public Team addTeam(@RequestBody Team team) {
-        return teamServiceIMPL.addTeam(team);
+        return iTeamService.addTeam(team);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteTeam(@PathVariable Long id) {
-        teamServiceIMPL.deleteTeam(id);
+        iTeamService.deleteTeam(id);
     }
 
     @PutMapping("/updateteams/{id}")
     public Team updateTeam(@PathVariable Long id, @RequestBody Team team) {
-        return teamServiceIMPL.updateTeam(id, team);
+        return iTeamService.updateTeam(id, team);
     }
 }
