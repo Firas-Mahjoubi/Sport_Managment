09e1bff585d6322e7a1d28c7152a5b60e61e2104
Firas-Mahjoubi <mahjoubi.firas@esprit.tn>
2025-04-27 20:06:54 +0100

    ai for the tactic

--- Sport_FrontEnd/SportManagment/src/app/app.module.ts
+++ Sport_FrontEnd/SportManagment/src/app/app.module.ts
@@ -93,6 +93,7 @@ import { TacticFolderComponent } from './tactics/tactic-folder/tactic-folder.com
 import { CreateTacticDialogComponent } from './tactics/create-tactic-dialog/create-tactic-dialog.component';
 import { TacticBoardComponent } from './tactics/tactic-board/tactic-board.component';
 import { SessionDetailsComponent } from './AdvancedPlanning/session-details/session-details.component';
+import { AiChatComponent } from './tactics/ai-chat/ai-chat.component';
 
 
 @NgModule({
@@ -153,6 +154,7 @@ import { SessionDetailsComponent } from './AdvancedPlanning/session-details/sess
     TacticBoardComponent,
     SessionDetailsComponent,
     HealthDashboardComponent,
+    AiChatComponent,
 
   ],
   imports: [
